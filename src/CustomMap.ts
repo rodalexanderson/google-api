44f4e162e3555db0952311e5ba7f72a74fb9d1c0
rodalexanderson <73920054+rodalexanderson@users.noreply.github.com>
2024-04-16 15:13:02 -0600

    commenting @types/google for bug fixing in deployment

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,4 +1,4 @@
-/// <reference types="@types/google.maps" />
+// /// <reference types="@types/google.maps" />
 
 // Instructions to other class on how to be an argument to 'addMarker'
 export interface Mappeable {
